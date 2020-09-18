10eebafdf40e334884db705980a45dba212633d1
Jacob Quon <jquon@sas.upenn.edu>
2020-09-18 11:16:37 -0400

    slurm testing

--- src/computeEpilogosWrite.py
+++ src/computeEpilogosWrite.py
@@ -13,37 +13,40 @@ def main(fileTag, outputDirectory, numStates):
 
     # Order matters to us when writing, so use sorted
     for file in sorted(outputDirPath.glob("temp_scores_{}_*.npy".format(fileTag))):
-        scoreArr = np.load(file, allow_pickle=False)
-        writeScores(fileTag, scoreArr, outputDirPath, numStates)
+        combinedArr = np.load(file, allow_pickle=False)
+        writeScores(fileTag, combinedArr, outputDirPath, numStates)
         
     # Clean up
     for file in sorted(outputDirPath.glob("temp_scores_{}_*.npy".format(fileTag))):
         os.remove(file)
 
 # Helper to write the final scores to files
-def writeScores(fileTag, scoreArr, outputDirPath, numStates):
+def writeScores(fileTag, combinedArr, outputDirPath, numStates):
     observationsTxtPath = outputDirPath / "observations_{}.txt.gz".format(fileTag)
     scoresTxtPath = outputDirPath / "scores_{}.txt.gz".format(fileTag)
 
     observationsTxt = gzip.open(observationsTxtPath, "wt")
     scoresTxt = gzip.open(scoresTxtPath, "wt")
 
+    scoreArr = combinedArr[:, 3:]
+    locationArr = combinedArr[:, 0:3]
+
     # Write each row in both observations and scores
     for i in range(scoreArr.shape[0]):
         # Write in the coordinates
-        for location in scoreArr[i, 0:3]:
+        for location in locationArr:
             observationsTxt.write("{}\t".format(location))
             scoresTxt.write("{}\t".format(location))
         
         # Write to observations
-        maxContribution = np.amax(scoreArr[i, 3:].astype(float))
-        maxContributionLoc = np.argmax(scoreArr[i, 3:].astype(float)) + 1
-        totalScore = np.sum(scoreArr[i, 3:].astype(float))
+        maxContribution = np.amax(scoreArr[i].astype(float))
+        maxContributionLoc = np.argmax(scoreArr[i].astype(float)) + 1
+        totalScore = np.sum(scoreArr[i].astype(float))
 
         observationsTxt.write("{}\t{:.5f}\t1\t{:.5f}\t\n".format(maxContributionLoc, maxContribution, totalScore))
 
         # Write to scores
-        for j in range(len(scoreArr[i, 3:])):
+        for j in range(len(scoreArr[i])):
             scoresTxt.write("{:.5f}\t".format(float(scoreArr[i, j])))
         scoresTxt.write("\n")
 

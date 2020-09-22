7753c26167d6f3b4c00397c419c6bf50c915f852
Jacob Quon <jquon@sas.upenn.edu>
2020-09-22 11:48:21 -0400

    testing full slurm write speed

--- src/computeEpilogosSlurmSaveTxt.py
+++ src/computeEpilogosSlurmSaveTxt.py
@@ -236,7 +236,7 @@ def main(fileDirectory, numStates, saliency, outputDirectory, calcExp, calcScore
 
         pythonCommand = "python {} {} {} {}".format(computeExpectedWritePy, fileTag, outputDirPath, numStates)
 
-        slurmCommand = "sbatch --dependency=afterok:{} --job-name={}.job --output={} --error={} --nodes=1 --ntasks=1 --mail-type=END --mail-user=jquon@altius.org --mem-per-cpu=16000 --wrap='{}'".format(jobIDStrWrite, jobName, jobOutPath, jobErrPath, pythonCommand)
+        slurmCommand = "sbatch --dependency=afterok:{} --job-name={}.job --output={} --error={} --nodes=1 --ntasks=1 --mail-type=END --mail-user=jquon@altius.org --mem-per-cpu=32000 --wrap='{}'".format(jobIDStrWrite, jobName, jobOutPath, jobErrPath, pythonCommand)
 
         sp = subprocess.run(slurmCommand, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
 

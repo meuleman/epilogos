537ceb4cde9aa73717ca3c04785c8e256dcd8d16
Jacob Quon <jquon@sas.upenn.edu>
2021-01-27 13:56:36 -0800

    reading testing

--- src/chromHMMConverter.py
+++ src/chromHMMConverter.py
@@ -52,8 +52,12 @@ def main(inputDir, outputDir):
         # loop over all rows which contain chromosome in question
         for row in directoryDF.loc[directoryDF["Chromosome"] == chromosome].itertuples():
             # Use the stored filename to read and store all but first 2 lines
+            print(row)
+            print(row.Filename)
             print(row[0])
             print(Path(row[0]))
+            print(row.Chromosome)
+            print(row.Genome)
             with gzip.open(row[0], "rb") as f:
                 stateList.append(f.readlines[2:])
         

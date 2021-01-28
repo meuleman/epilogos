97b56b19c01ce5a124843279834abe75b5ac431f
Wouter Meuleman <meuleman@altius.org>
2021-01-28 10:48:19 -0800

    Added the use of 'stat' to find the right files in gen_input_data.sh

--- scripts/gen_input_data.sh
+++ scripts/gen_input_data.sh
@@ -14,7 +14,8 @@ for CHR in $(cut -f1 ${GENOME}.genome); do
   echo "Processing ${CHR}..."
   CMD="paste"; 
   for BIOSAMPLE in $(cut -f1 ${METADATA} | tail -n +2); do
-    CMD="$CMD <(zcat -f -- ${DIR}/*${BIOSAMPLE}*${CHR}_*.txt?(.gz))"; 
+    NAM=`stat -t -c %n ${DIR}/*${BIOSAMPLE}*${CHR}_*.txt*`
+    CMD="$CMD <(zcat -f -- ${NAM})";
   done
   eval $CMD | awk 'BEGIN{OFS="\t";chr=""}
     {if (NR==1) { chr=$2 } else { if (NR>2) print chr, (NR-3)*200, (NR-2)*200, $0}}' \

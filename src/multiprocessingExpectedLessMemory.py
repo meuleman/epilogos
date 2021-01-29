130d938ffe8c656fbc96902592cd9b99c861034a
Jacob Quon <jquon@sas.upenn.edu>
2021-01-29 11:22:35 -0800

    New multiprocessing system

---
+++ src/multiprocessingExpectedLessMemory.py
@@ -0,0 +1,220 @@
+import numpy as np
+import sys
+from pathlib import Path
+import math
+import pandas as pd
+import time
+import operator as op
+from functools import reduce
+import multiprocessing
+import itertools
+from contextlib import closing
+
+def main(filename, numStates, saliency, outputDirPath, fileTag, numProcesses):
+    dataFilePath = Path(filename)
+    outputDirPath = Path(outputDirPath)
+
+    ###################################################
+    ##
+    ##   chrName is hacky, find a better way later
+    ##
+    ####################################################
+    chrName  = dataFilePath.name.split("_")[1].split(".")[0]
+
+
+    # If saliency is 1, we don't have to worry about all the multiprocessing stuff
+    if saliency == 1:
+        s1Exp(dataFilePath, numStates, outputDirPath, fileTag, chrName)
+        return
+
+    # Get the genome file
+    genomeFileList = dataFilePath.parents[0].glob("*.genome")
+    if len(genomeFileList) > 1:
+        raise IOError("Too many '.genome' files provided")
+    elif len(genomeFileList) < 1:
+        raise IOError("No '.genome' file provided")
+
+    # Read line by line until we are on correct chromosome, then read out number of bp
+    for genomeFile in genomeFileList:
+        with open(genomeFile, "r") as gf:
+            line = gf.readline()
+            while chrName not in line:
+                line = gf.readline
+            basePairs = int(line.split()[1])
+
+    numRows = int(basePairs / 200)
+
+    # If user doesn't want to choose number of cores use as many as available
+    if numProcesses == 0:
+        numProcesses = multiprocessing.cpu_count()
+
+    # Split the rows up according to the number of cores we have available
+    rowList = []
+    for i in range(numProcesses):
+        rowsToCalculate = (i * numRows // numProcesses, (i+1) * numRows // numProcesses)
+        rowList.append(rowsToCalculate)
+
+    # Determine saliency and send to corresponding function
+    if saliency == 2:
+        s2Exp(dataFilePath, rowList, numRows, numStates, outputDirPath, fileTag, chrName, numProcesses)
+    elif saliency == 3:
+        s3Exp(dataFilePath, rowList, numRows, numStates, outputDirPath, fileTag, chrName, numProcesses)
+    else:
+        print("Inputed saliency value not supported")
+        return
+
+
+# Function that calculates the expected frequencies for the S1 metric
+def s1Exp(dataFilePath, numStates, outputDirPath, fileTag, chrName):
+    # Reading in the data
+    print("\nReading data from file...")
+    tRead = time.time()
+    dataDF = pd.read_table(dataFilePath, header=None, sep="\t")
+    print("    Time: ", time.time() - tRead)
+
+    # Converting to a np array for faster functions later
+    print("Converting to numpy array...")
+    tConvert = time.time()
+    dataArr = dataDF.iloc[:,3:].to_numpy(dtype=int) - 1 
+    print("    Time: ", time.time() - tConvert)
+
+    numRows, numCols = dataArr.shape
+
+    # Calculate the expected frequencies of each state
+    # Done column-wise instead of row-wise thus we don't multiprocess as it provides negligible efficiency bonus
+    stateIndices = list(range(1, numStates + 1))
+    expFreqSeries = pd.Series(np.zeros(numStates), index=stateIndices)
+    dfSize = numRows * numCols
+    for i in range(3, numCols + 3):
+        stateCounts = dataDF[i].value_counts()
+        for state, count in stateCounts.items():
+            expFreqSeries.loc[state] += count / dfSize
+    expFreqArr = expFreqSeries.to_numpy(dtype=np.float32)
+
+    storeExpArray(expFreqArr, outputDirPath, fileTag, chrName)
+
+
+# Function that deploys the processes used to calculate the expected frequencies for the s2 metric. Also calls function to store expected frequency
+def s2Exp(dataFilePath, rowList, numRows, numStates, outputDirPath, fileTag, chrName, numProcesses):
+    print("NUM PROCESSES:", numProcesses)
+
+    if (sys.version_info < (3, 8)):
+        print("\nFor maximum efficiency please update python to version 3.8 or later")
+        print("NOTE: The code will still run in a lower version, but will be slightly slower\n")
+
+    # Start the processes
+    with closing(multiprocessing.Pool(numProcesses)) as pool:
+        results = pool.starmap(s2Calc, zip(itertools.repeat(dataFilePath), rowList, itertools.repeat(numStates)))
+    pool.join()
+
+    # Sum all the expected frequency arrays from the seperate processes and normalize by dividing by numRows
+    expFreqArr = np.sum(results, axis = 0) / numRows)
+
+    storeExpArray(expFreqArr, outputDirPath, fileTag, chrName)
+
+
+# Function that reads in data and calculates the expected frequencies for the S2 metric over a chunk of the data
+def s2Calc(dataFilePath, rowsToCalculate, numStates):
+    # Reading in the data
+    print("\nReading data from file...")
+    tRead = time.time()
+    dataDF = pd.read_table(dataFilePath, skiprows=rowsToCalculate[0], nrows=rowsToCalculate[1]-rowsToCalculate[0], header=None, sep="\t")
+    print("    Time: ", time.time() - tRead)
+
+    numRows, numCols = dataDF.shape
+
+    # Converting to a np array for faster functions
+    print("Converting to numpy array...")
+    tConvert = time.time()
+    dataArr = dataDF.iloc[:,3:].to_numpy(dtype=int) - 1 
+    print("    Time: ", time.time() - tConvert)
+
+    expFreqArr = np.zeros((numStates, numStates), dtype=np.float32)
+
+    # SumOverRows: (Within a row, how many ways can you choose x and y to be together) / (how many ways can you choose 2 states)
+    # SumOverRows: (Prob of choosing x and y)
+    # Can choose x and y to be together x*y ways if different and n(n-1)/2 ways if same (where n is the number of times that x/y shows up)
+    if (sys.version_info < (3, 8)):
+        combinations = ncr(numCols, 2)
+        for row in range(len(rowsToCalculate)):
+            uniqueStates, stateCounts = np.unique(dataArr[row], return_counts=True)
+            for i in range(len(uniqueStates)):
+                for j in range(len(uniqueStates)):
+                    if uniqueStates[i] > uniqueStates[j] or uniqueStates[i] < uniqueStates[j]:
+                        expFreqArr[uniqueStates[i], uniqueStates[j]] += stateCounts[i] * stateCounts[j] / combinations / 2 # Extra 2 is to account for the symmetric matrix
+                    elif uniqueStates[i] == uniqueStates[j]:
+                        expFreqArr[uniqueStates[i], uniqueStates[j]] += ncr(stateCounts[i], 2) / combinations
+    else:
+        combinations = math.comb(numCols, 2)
+        for row in  range(len(rowsToCalculate)):
+            uniqueStates, stateCounts = np.unique(dataArr[row], return_counts=True) 
+            for i in range(len(uniqueStates)):
+                for j in range(len(uniqueStates)):
+                    if uniqueStates[i] > uniqueStates[j] or uniqueStates[i] < uniqueStates[j]:
+                        expFreqArr[uniqueStates[i], uniqueStates[j]] += stateCounts[i] * stateCounts[j] / combinations / 2 # Extra 2 is to account for the symmetric matrix
+                    elif uniqueStates[i] == uniqueStates[j]:
+                        expFreqArr[uniqueStates[i], uniqueStates[j]] += math.comb(stateCounts[i], 2) / combinations
+
+    return expFreqArr
+
+# Function that deploys the processes used to calculate the expected frequencies for the s3 metric. Also call function to store expected frequency
+def s3Exp(dataFilePath, rowList, numRows, numStates, outputDirPath, fileTag, chrName, numProcesses):
+    print("NUM PROCESSES:", numProcesses)
+
+    # Start the processes
+    with closing(multiprocessing.Pool(numProcesses)) as pool:
+        results = pool.starmap(s3Calc, zip(itertools.repeat(dataFilePath), rowList, itertools.repeat(numStates)))
+    pool.join()
+
+    # Read one line to determine numCols
+    numCols = pd.read_table(dataFilePath, nrows=1, header=None, sep="\t").shape[1]
+
+    # Sum all the expected frequency arrays from the seperate processes and normalize by dividing
+    expFreqArr = np.sum(results, axis = 0) / numRows * numCols * (numCols - 1)
+
+    storeExpArray(expFreqArr, outputDirPath, fileTag, chrName)
+
+# Function that calculates the expected frequencies for the S3 metric over a chunk of the data
+def s3Calc(dataFilePath, rowsToCalculate, numStates):
+    # Reading in the data
+    print("\nReading data from file...")
+    tRead = time.time()
+    dataDF = pd.read_table(dataFilePath, skiprows=rowsToCalculate[0], nrows=rowsToCalculate[1]-rowsToCalculate[0], header=None, sep="\t")
+    print("    Time: ", time.time() - tRead)
+
+    numRows, numCols = dataDF.shape
+
+    # Converting to a np array for faster functions
+    print("Converting to numpy array...")
+    tConvert = time.time()
+    dataArr = dataDF.iloc[:,3:].to_numpy(dtype=int) - 1 
+    print("    Time: ", time.time() - tConvert)
+
+    # Gives us everyway to combine the column numbers in numpy indexing form
+    basePermutationArr = np.array(list(itertools.permutations(range(numCols), 2))).T
+
+    expFreqArr = np.zeros((numCols, numCols, numStates, numStates), dtype=np.int32)
+    
+    # We tally a one for all the state/column combinations we observe (e.g. for state 18 in column 2 and state 15 in column 6 we would add one to index [5, 1, 17, 14])
+    for row in numRows:
+        expFreqArr[basePermutationArr[0], basePermutationArr[1], dataArr[row, basePermutationArr[0]], dataArr[row, basePermutationArr[1]]] += np.ones(basePermutationArr.shape[1], dtype=np.int32)
+
+    return expFreqArr
+
+# Helper to store the expected frequency arrays
+def storeExpArray(expFreqArr, outputDirPath, fileTag, chrName):
+    # Creating a file path
+    expFreqFilename = "temp_exp_freq_{}_{}.npy".format(fileTag, chrName)
+    expFreqPath = outputDirPath / expFreqFilename
+
+    np.save(expFreqPath, expFreqArr, allow_pickle=False)
+
+# Helper to calculate combinations
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
+    return numer // denom
+
+if __name__ == "__main__":
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), sys.argv[4], sys.argv[5], int(sys.argv[6]))
---
+++ src/multiprocessingSlurm.py
@@ -0,0 +1,326 @@
+import gzip
+import numpy as np
+from pathlib import Path
+import pandas as pd
+import time
+import click
+import os
+import subprocess
+from pathlib import PurePath
+
+@click.command()
+@click.option("-f", "--file-directory", "fileDirectory", type=str, required=True, help="Path to directory that contains files to read from (All files in this directory will be read in)")
+@click.option("-o", "--output-directory", "outputDirectory", type=str, required=True, help="Output Directory (CANNOT be the same as input directory)\n")
+@click.option("-s", "--state-model", "numStates", type=int, required=True, help="Number of states in chromatin state model")
+@click.option("-l", "--saliency-level", "saliency", type=int, default=1, show_default=True, help="Desired saliency level (1, 2, or 3)")
+@click.option("-m", "--mode-of-operation", "modeOfOperation", type=click.Choice(["bg", "s", "both"]), default="both", show_default=True, help="bg for background, s for scores, both for both")
+@click.option("-b", "--background-directory", "expFreqDir", type=str, default="null", help="Path to where the background frequency array is read from (-m s) or written to (-m bg, -m both) [default: output-directory]")
+@click.option("-c", "--num-cores", "numProcesses", type=int, default=0, help="The number of cores to run on [default: 0 = Uses all cores]")
+@click.option("-x", "--exit-when-complete", "exitBool", is_flag=True, help="If flag is enabled program will exit upon completion of all processes rather than SLURM submission of all processes")
+def main(fileDirectory, numStates, saliency, outputDirectory, modeOfOperation, expFreqDir, numProcesses, exitBool):
+    """
+    This script computes scores for chromatin states across the genome.
+    """
+    dataFilePath = Path(fileDirectory)
+    outputDirPath = Path(outputDirectory)
+
+    print()
+    print("Input Directory =", dataFilePath)
+    print("State Model =", numStates)
+    print("Saliency level =", saliency)
+    print("Output Directory =", outputDirPath)
+    print("Mode of Operation =", modeOfOperation)
+    print("Background Directory =", expFreqDir)
+
+    # If user does not specificy a directory to look for expected frequencies default the output directory
+    if expFreqDir == "null":
+        expFreqDir = outputDirectory
+
+    # Making paths absolute
+    if not PurePath(outputDirPath).is_absolute():
+        outputDirPath = Path.cwd() / outputDirPath
+    if not PurePath(dataFilePath).is_absolute():
+        dataFilePath = Path.cwd() / dataFilePath
+
+    # For making sure all files are consistently named
+    fileTag = "{}_saliency{}".format(dataFilePath.name, saliency)
+
+    if saliency != 1 and saliency != 2 and saliency != 3:
+        print("\nERROR: Ensure that saliency metric is either 1, 2, or 3\n")
+        return
+
+    # Check that paths are valid before doing anything
+    if not dataFilePath.exists() or not dataFilePath.is_dir():
+        print("\nERROR: Given file path does not exist or is not a directory\n")
+        return
+
+    if not list(dataFilePath.glob("*")):
+        print("\nERROR: Ensure that file directory is not empty\n")
+        return
+
+    # If the output directory does not exist yet, make it for the user 
+    if not outputDirPath.exists():
+        outputDirPath.mkdir(parents=True)
+    
+    if not outputDirPath.is_dir():
+        print("\nERROR: Output directory is not a directory\n")
+        return
+
+    if numProcesses < 0:
+        print("\nERROR: Number of cores must be positive or zero (0 means use all cores)\n")
+        return
+    elif numProcesses == 0:
+        numTasks = "--exclusive"
+    else:
+        numTasks = "--ntasks={}".format(numProcesses)
+
+    # For slurm output and error later
+    (outputDirPath / ".out/").mkdir(parents=True, exist_ok=True)
+    (outputDirPath / ".err/").mkdir(parents=True, exist_ok=True)
+
+    # Path for storing/retrieving the expected frequency array
+    # Expected frequency arrays are stored according to path of the input file directory
+    expFreqFilename = "exp_freq_{}.npy".format(fileTag)
+    storedExpPath = Path(expFreqDir) / expFreqFilename
+    print("\nBackground Frequency Array Location:", storedExpPath)
+
+    # Finding the location of the .py files that must be run
+    if PurePath(__file__).is_absolute():
+        pythonFilesDir = Path(__file__).parents[0]
+    else:
+        pythonFilesDir = Path.cwd() / Path(__file__).parents[0]
+
+    # Only calculate the expected frequencies if user asks for it, otherwise just load from where the user said
+    if modeOfOperation == "s":
+        try:
+            expFreqArr = np.load(storedExpPath, allow_pickle=False)
+        except IOError:
+            print("\nERROR: Could not load stored expected value array.\n\tPlease check that the directory is correct or that the file exists\n")
+            return
+    else:     
+        expJobIDArr = []   
+        print("\nSubmitting Slurm Jobs for Per Datafile Background Frequency Calculation....")
+        for file in dataFilePath.glob("*"):
+            if file.name.split(".")[1] == "genome":
+                continue
+            if not file.is_dir():
+                filename = file.name.split(".")[0]
+                jobName = "exp_freq_calc_{}_{}".format(fileTag, filename)
+                jobOutPath = outputDirPath / (".out/" + jobName + ".out")
+                jobErrPath = outputDirPath / (".err/" + jobName + ".err")
+
+                # Creating the out and err files for the batch job
+                if jobOutPath.exists():
+                    os.remove(jobOutPath)
+                if jobErrPath.exists():
+                    os.remove(jobErrPath)
+                try:
+                    jout = open(jobOutPath, 'x')
+                    jout.close()
+                    jerr = open(jobErrPath, 'x')
+                    jerr.close()
+                except FileExistsError:
+                    # This error should never occur because we are deleting the files first
+                    print("\nERROR: sbatch '.out' or '.err' file already exists\n")
+                    return
+
+                # computeExpectedPy = pythonFilesDir / "computeEpilogosExpected.py"
+
+                computeExpectedPy = pythonFilesDir / "expectedMultiprocessing.py"
+
+                pythonCommand = "python {} {} {} {} {} {} {}".format(computeExpectedPy, file, numStates, saliency, outputDirPath, fileTag, numProcesses)
+
+                if saliency == 1:
+                    slurmCommand = "sbatch --job-name={}.job --output={} --error={} --ntasks=1 --mem-per-cpu=32000 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, pythonCommand)
+                elif saliency == 2:
+                    slurmCommand = "sbatch --job-name=S2_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                elif saliency == 3:
+                    slurmCommand = "sbatch --job-name=S3_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+
+                sp = subprocess.run(slurmCommand, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+
+                if not sp.stdout.startswith("Submitted batch"):
+                    print("\nERROR: sbatch not submitted correctly\n")
+                    return
+                
+                expJobIDArr.append(int(sp.stdout.split()[-1]))
+
+        # Combining all the different chromosome expected frequency arrays into one
+        # create a string for slurm dependency to work
+        expJobIDStr = str(expJobIDArr).strip('[]').replace(" ", "")
+
+        print("    JobIDs:", expJobIDStr)
+
+        print("\nSubmitting Slurm Job for Combining Background Frequency Arrays....")
+
+        jobName = "exp_freq_comb_{}".format(fileTag)
+        jobOutPath = outputDirPath / (".out/" + jobName + ".out")
+        jobErrPath = outputDirPath / (".err/" + jobName + ".err")
+
+        # Creating the out and err files for the batch job
+        if jobOutPath.exists():
+            os.remove(jobOutPath)
+        if jobErrPath.exists():
+            os.remove(jobErrPath)
+        try:
+            jout = open(jobOutPath, 'x')
+            jout.close()
+            jerr = open(jobErrPath, 'x')
+            jerr.close()
+        except FileExistsError:
+            # This error should never occur because we are deleting the files first
+            print("\nERROR: sbatch '.out' or '.err' file already exists\n")
+            return
+
+        computeExpectedCombinationPy = pythonFilesDir / "computeEpilogosExpectedCombination.py"
+
+        pythonCommand = "python {} {} {} {}".format(computeExpectedCombinationPy, outputDirPath, fileTag, storedExpPath)
+
+        if saliency == 1:
+            slurmCommand = "sbatch --dependency=afterok:{} --job-name={}.job --output={} --error={} --ntasks=1 --mem-per-cpu=8000 --wrap='{}'".format(expJobIDStr, jobName, jobOutPath, jobErrPath, pythonCommand)
+        elif saliency == 2:
+            slurmCommand = "sbatch --dependency=afterok:{} --job-name=S2_{}.job --output={} --error={} --ntasks=1 --mem-per-cpu=8000 --wrap='{}'".format(expJobIDStr, jobName, jobOutPath, jobErrPath, pythonCommand)
+        elif saliency == 3:
+            slurmCommand = "sbatch --dependency=afterok:{} --job-name=S3_{}.job --output={} --error={} --ntasks=1 --mem-per-cpu=8000 --wrap='{}'".format(expJobIDStr, jobName, jobOutPath, jobErrPath, pythonCommand)
+
+        sp = subprocess.run(slurmCommand, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+
+        if not sp.stdout.startswith("Submitted batch"):
+            print("\nERROR: sbatch not submitted correctly\n")
+            return
+        
+        combinationJobID = int(sp.stdout.split()[-1])
+
+        print("    JobID:", combinationJobID)
+
+    if modeOfOperation == "s" or modeOfOperation == "both":
+        # Calculate the observed frequencies and scores
+        print("\nSubmitting Slurm Jobs for Score Calculation....")
+        scoreJobIDArr = []
+        for file in dataFilePath.glob("*"):
+            if file.name.split(".")[1] == "genome":
+                continue
+            if not file.is_dir():
+                filename = file.name.split(".")[0]
+                jobName = "score_calc_{}_{}".format(fileTag, filename)
+                jobOutPath = outputDirPath / (".out/" + jobName + ".out")
+                jobErrPath = outputDirPath / (".err/" + jobName + ".err")
+
+                # Creating the out and err files for the batch job
+                if jobOutPath.exists():
+                    os.remove(jobOutPath)
+                if jobErrPath.exists():
+                    os.remove(jobErrPath)
+                try:
+                    jout = open(jobOutPath, 'x')
+                    jout.close()
+                    jerr = open(jobErrPath, 'x')
+                    jerr.close()
+                except FileExistsError:
+                    # This error should never occur because we are deleting the files first
+                    print("\nERROR: sbatch '.out' or '.err' file already exists\n")
+                    return
+                
+                # computeScorePy = pythonFilesDir / "computeEpilogosScores.py"
+
+                computeScorePy = pythonFilesDir / "multiprocessingPlayground.py"
+
+                pythonCommand = "python {} {} {} {} {} {} {} {}".format(computeScorePy, file, numStates, saliency, outputDirPath, storedExpPath, fileTag, numProcesses)
+
+                if modeOfOperation == "s":
+                    if saliency == 1:
+                        slurmCommand = "sbatch --job-name={}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                    elif saliency == 2:
+                        slurmCommand = "sbatch --job-name=S2_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                    elif saliency == 3:
+                        slurmCommand = "sbatch --job-name=S3_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                else:
+                    if saliency == 1:
+                        slurmCommand = "sbatch --dependency=afterok:{} --job-name={}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(combinationJobID, jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                    elif saliency == 2:
+                        slurmCommand = "sbatch --dependency=afterok:{} --job-name=S2_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(combinationJobID, jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+                    elif saliency == 3:
+                        slurmCommand = "sbatch --dependency=afterok:{} --job-name=S3_{}.job --output={} --error={} {} --mem=0 --wrap='{}'".format(combinationJobID, jobName, jobOutPath, jobErrPath, numTasks, pythonCommand)
+
+                sp = subprocess.run(slurmCommand, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+
+                if not sp.stdout.startswith("Submitted batch"):
+                    print("\nERROR: sbatch not submitted correctly\n")
+                    return
+                
+                scoreJobIDArr.append(int(sp.stdout.split()[-1]))
+
+        # Create a string for slurm dependency to work
+        scoreJobIDStr = str(scoreJobIDArr).strip('[]').replace(" ", "")
+        
+        print("    JobIDs:", scoreJobIDStr)
+
+        # Write scores out to gzipped text files
+        print("\nSubmitting Slurm Jobs for Writing to Score Files....")
+        writeJobIDArr = []
+        for file in dataFilePath.glob("*"):
+            if file.name.split(".")[1] == "genome":
+                continue
+            if not file.is_dir():
+                filename = file.name.split(".")[0]
+                jobName = "writeFaster_{}_{}".format(fileTag, filename)
+                jobOutPath = outputDirPath / (".out/" + jobName + ".out")
+                jobErrPath = outputDirPath / (".err/" + jobName + ".err")
+
+                # Creating the out and err files for the batch job
+                if jobOutPath.exists():
+                    os.remove(jobOutPath)
+                if jobErrPath.exists():
+                    os.remove(jobErrPath)
+                try:
+                    jout = open(jobOutPath, 'x')
+                    jout.close()
+                    jerr = open(jobErrPath, 'x')
+                    jerr.close()
+                except FileExistsError:
+                    # This error should never occur because we are deleting the files first
+                    print("\nERROR: sbatch '.out' or '.err' file already exists\n")
+                    return
+
+                computeExpectedWritePy = pythonFilesDir / "computeEpilogosWrite.py"
+
+                pythonCommand = "python {} {} {} {} {}".format(computeExpectedWritePy, filename, numStates, outputDirPath, fileTag)
+
+                slurmCommand = "sbatch --dependency=afterok:{} --job-name={}.job --output={} --error={} --ntasks=1 --mem-per-cpu=64000 --wrap='{}'".format(scoreJobIDStr, jobName, jobOutPath, jobErrPath, pythonCommand)
+
+                sp = subprocess.run(slurmCommand, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+
+                if not sp.stdout.startswith("Submitted batch"):
+                    print("\nERROR: sbatch not submitted correctly\n")
+                    return
+
+                writeJobIDArr.append(int(sp.stdout.split()[-1]))
+
+        writeJobIDStr = str(writeJobIDArr).strip('[]').replace(" ", "")
+        print("    JobIDs:", writeJobIDStr)
+        if modeOfOperation == "both":
+            print("\nAll JobIDs: {},{},{},{}".format(expJobIDStr, combinationJobID, scoreJobIDStr, writeJobIDStr))
+        elif modeOfOperation == "s":
+            print("\nAll JobIDs: {},{}".format(scoreJobIDStr, writeJobIDStr))
+        elif modeOfOperation == "bg":
+            print("\nAll JobIDs: {},{}".format(expJobIDStr, combinationJobID))
+
+    # If the user wants to exit upon job completion rather than submission
+    if exitBool:
+        # The last job is different depending on mode of operation
+        if modeOfOperation == "s" or modeOfOperation == "both":
+            lastJob = writeJobIDArr[-1]
+        else:
+            lastJob = combinationJobID
+
+        lastJobCheckStr = "sacct --format=State --jobs {}".format(lastJob)
+
+        # Every ten seconds check if the final job is done, if it is exit the program
+        while True:
+            time.sleep(10)
+            sp = subprocess.run(lastJobCheckStr, shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+            if not (sp.stdout.strip().endswith("RUNNING") or sp.stdout.strip().endswith("PENDING")):
+                break
+                
+if __name__ == "__main__":
+    main()

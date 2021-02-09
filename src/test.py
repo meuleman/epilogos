22ac5aa44093a8125be53a036bbfc41449755c10
Jacob Quon <jquon@sas.upenn.edu>
2021-02-09 12:54:30 -0800

    new s2

--- src/test.py
+++ src/test.py
@@ -19,7 +19,7 @@ import gzip
 import numpy.ma as ma
 import click
 
-def main(file1, file2, numStates):
+def main():
     
     # file1Path = Path(file1)
     # file2Path = Path(file2)
@@ -76,62 +76,62 @@ def main(file1, file2, numStates):
 
 
 
-    print("\nReading python expFreqARr")
-    pythonArr = np.load(Path(file1), allow_pickle=False)
+    # print("\nReading python expFreqARr")
+    # pythonArr = np.load(Path(file1), allow_pickle=False)
 
-    print("\nReading c expFreqARr")
-    tRead1 = time.time()
-    file2DF = pd.read_table(Path(file2), header=None, sep="\t")
-    print("    Time: ", time.time() - tRead1)
+    # print("\nReading c expFreqARr")
+    # tRead1 = time.time()
+    # file2DF = pd.read_table(Path(file2), header=None, sep="\t")
+    # print("    Time: ", time.time() - tRead1)
 
-    print("Converting to numpy arrays...")
-    tConvert = time.time()
-    file2Arr = file2DF.to_numpy(dtype=int)
-    print("    Time: ", time.time() - tConvert)
+    # print("Converting to numpy arrays...")
+    # tConvert = time.time()
+    # file2Arr = file2DF.to_numpy(dtype=int)
+    # print("    Time: ", time.time() - tConvert)
 
-    print(file2Arr.shape)
+    # print(file2Arr.shape)
 
-    numCols = 127
+    # numCols = 127
 
-    epigenomeCombinations = np.array(list(itertools.combinations(range(numCols), 2)), dtype=np.int16).T
-    stateCombinations = np.array(list(itertools.product(range(numStates), repeat=2)), dtype=np.int16).T
+    # epigenomeCombinations = np.array(list(itertools.combinations(range(numCols), 2)), dtype=np.int16).T
+    # stateCombinations = np.array(list(itertools.product(range(numStates), repeat=2)), dtype=np.int16).T
 
-    cArr = np.zeros((numCols, numCols, numStates, numStates), dtype=np.float32)
+    # cArr = np.zeros((numCols, numCols, numStates, numStates), dtype=np.float32)
 
-    # loop over all rows in erics txt file
-    print("putting txt into numpy array...")
-    for i in range(file2Arr.shape[0]):
-        for j in range(file2Arr.shape[1]):
-            cArr[epigenomeCombinations[0, i], epigenomeCombinations[1, i], stateCombinations[0, j], stateCombinations[1, j]] = file2Arr[i, j]
-            cArr[epigenomeCombinations[1, i], epigenomeCombinations[0, i], stateCombinations[0, j], stateCombinations[1, j]] = file2Arr[i, j]
+    # # loop over all rows in erics txt file
+    # print("putting txt into numpy array...")
+    # for i in range(file2Arr.shape[0]):
+    #     for j in range(file2Arr.shape[1]):
+    #         cArr[epigenomeCombinations[0, i], epigenomeCombinations[1, i], stateCombinations[0, j], stateCombinations[1, j]] = file2Arr[i, j]
+    #         cArr[epigenomeCombinations[1, i], epigenomeCombinations[0, i], stateCombinations[0, j], stateCombinations[1, j]] = file2Arr[i, j]
     
-    cArr /= (15181508 * numCols * (numCols - 1))
+    # cArr /= (15181508 * numCols * (numCols - 1))
 
 
-    print(pythonArr[0,1])
-    print(cArr[0, 1])
+    # print(pythonArr[0,1])
+    # print(cArr[0, 1])
 
 
 
 
 
-    # print("Calculating top 100...")
-    # diffArr = np.abs(pythonArr - cArr)
+    # # print("Calculating top 100...")
+    # # diffArr = np.abs(pythonArr - cArr)
 
-    # sortedIndices = np.argsort(-diffArr, axis=None)[:100]
+    # # sortedIndices = np.argsort(-diffArr, axis=None)[:100]
 
-    # fourDIndices = np.unravel_index(sortedIndices, (numCols, numCols, numStates, numStates))
+    # # fourDIndices = np.unravel_index(sortedIndices, (numCols, numCols, numStates, numStates))
 
-    flatPython = pythonArr.flatten()
-    flatC = cArr.flatten()
-    # flatDiff = diffArr.flatten()
+    # flatPython = pythonArr.flatten()
+    # flatC = cArr.flatten()
+    # # flatDiff = diffArr.flatten()
 
 
-    sortFlatPython = np.sort(flatPython)
-    sortFlatC = np.sort(flatC)
+    # sortFlatPython = np.sort(flatPython)
+    # sortFlatC = np.sort(flatC)
 
-    print(sortFlatPython[:50])
-    print(sortFlatC[:50])
+    # print(sortFlatPython[:50])
+    # print(sortFlatC[:50])
 
 
     # for i in range(100):
@@ -179,7 +179,80 @@ def main(file1, file2, numStates):
     # print("8.", np.where((genomeArr[:, 69] == 5) & (genomeArr[:, 27] == 9))[0])
 
     
+    inputPath = Path("/home/jquon/AdseraStateByGroup/male/matrix.txt.gz")
+
+    dataDF = pd.read_table(inputPath, nrows=10000, header=None, sep="\t")
+
+    dataArr = dataDF.iloc[:,3:].to_numpy(dtype=int) - 1 
+
+    numStates = 18
+
+    ncrTimes = []
+
+    numRows, numCols = dataArr.shape
+
+    for i in range(10):
+        expFreqArr = np.zeros((numStates, numStates), dtype=np.float32)
+
+        tncr = time.time()
+        combinations = ncr(numCols, 2)
+        for row in range(numRows):
+            uniqueStates, stateCounts = np.unique(dataArr[row], return_counts=True)
+            for i, state1 in enumerate(uniqueStates):
+                for j, state2 in enumerate(uniqueStates):
+                    if state1 == state2:
+                        expFreqArr[state1, state2] += ncr(stateCounts[i], 2) / combinations
+                    else: # state1 > state2 or state1 < state2
+                        expFreqArr[state1, state2] += stateCounts[i] * stateCounts[j] / combinations / 2 # Extra 2 is to account for the symmetric matrix
+
+        ncrTimes.append(time.time() - tncr)
+
+    combTimes = []
+
+    for i in range(10):
+        expFreqArr = np.zeros((numStates, numStates), dtype=np.float32)
+
+        tcomb = time.time()
+        combinations = math.comb(numCols, 2)
+        for row in range(numRows):
+            uniqueStates, stateCounts = np.unique(dataArr[row], return_counts=True) 
+            for i, state1 in enumerate(uniqueStates):
+                for j, state2 in enumerate(uniqueStates):
+                    if state1 == state2:
+                        expFreqArr[state1, state2] += math.comb(stateCounts[i], 2) / combinations
+                    else: # state1 > state2 or state1 < state2
+                        expFreqArr[state1, state2] += stateCounts[i] * stateCounts[j] / combinations / 2
+
+        combTimes.append(time.time() - tcomb)
+    
+    newTimes = []
+    for i in range(10):
+        expFreqArr = np.zeros((numStates, numStates), dtype=np.int32)
+
+        tnew = time.time()
+        basePermutationArr = np.array(list(itertools.permutations(range(numCols), 2))).T
+        for row in range(numRows):
+            expFreqArr[dataArr[row, basePermutationArr[0]], dataArr[row, basePermutationArr[1]]] += 1
+            
+        newTimes.append(time.time() - tnew)
+
+
+    print("NCR: ", ncrTimes)
+    print("    Mean:", sum(ncrTimes) / len(ncrTimes))
+
+    print("Comb: ", combTimes)
+    print("    Mean:", sum(combTimes) / len(combTimes))
+
+    print("NEW: ", newTimes)
+    print("    Mean:", sum(newTimes) / len(newTimes))
+
+# Helper to calculate combinations
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
+    return numer // denom
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+    main()

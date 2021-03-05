d90a627b638390f1577f197a733682a7c23e4949
Jacob Quon <jquon@sas.upenn.edu>
2021-03-05 13:43:16 -0500

    bootstrapping test

--- src/test.py
+++ src/test.py
@@ -57,27 +57,27 @@ def main(file):
     pairwiseMetrics = Path(file)
     # pairwiseMetrics   = Path("/home/jquon/bootstrappingTest/100000/pairwiseMetrics_mmo_fmo_s1.txt.gz")
     names = ['chr', 'binStart', 'binEnd', 'distance']
-    distanceDF = pd.read_table(pairwiseMetrics, usecols=[0, 1, 2, 4], header=None, sep="\t", names=names)
-    distanceArr = np.array(distanceDF['distance'])
+    distanceDF = pd.read_table(pairwiseMetrics, usecols=[0, 1, 2, 3], header=None, sep="\t", names=names)
+    # distanceArr = np.array(distanceDF['distance'])
 
-    idx = [i for i in range(len(distanceArr)) if round(distanceArr[i], 5) != 0]
-    dataNull = pd.Series(distanceArr[idx])
+    # idx = [i for i in range(len(distanceArr)) if round(distanceArr[i], 5) != 0]
+    # dataNull = pd.Series(distanceArr[idx])
 
-    # Multiprocess the reading
-    with closing(Pool(numProcesses)) as pool:
-        results = pool.starmap(test, zip(itertools.repeat(dataNull, 1001), itertools.repeat(1000, 1001)))
-    pool.join()
+    # # Multiprocess the reading
+    # with closing(Pool(numProcesses)) as pool:
+    #     results = pool.starmap(test, zip(itertools.repeat(dataNull, 1001), itertools.repeat(1000, 1001)))
+    # pool.join()
 
-    index = [i for i in range(len(results))]
-    columns = [i for i in range(1000)]
+    # index = [i for i in range(len(results))]
+    # columns = [i for i in range(1000)]
 
-    randomDF = pd.DataFrame(index=index, columns=columns)
+    # randomDF = pd.DataFrame(index=index, columns=columns)
 
-    for i in range(len(results)):
-        randomDF.iloc[i] = list(results[i])
+    # for i in range(len(results)):
+    #     randomDF.iloc[i] = list(results[i])
 
     print("unique distances", len(distanceDF['distance'].unique()))
-    print("Unique values", len(randomDF.iloc[:,0].unique()))
+    # print("Unique values", len(randomDF.iloc[:,0].unique()))
 
 def test(x, size):
     np.random.seed()

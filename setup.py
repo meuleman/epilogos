f3a12656f7c6c0e8646824bc9f5ce527e78da929
Jacob Quon <jquon@sas.upenn.edu>
2021-05-17 16:11:41 -0400

    setup added

---
+++ setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='epilogos',
+    version='1.0.0',
+    description="Information-theoretic navigation of multi-tissue functional genomic annotations",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meuleman/epilogos",
+    authors=['Wouter Meuleman', 'Jacob Quon', 'Alex Reynolds', 'Eric Rynes'],
+    license='GNU General Public License v3.0',
+    packages=['src'],
+    install_requires=['click', 'numpy', 'pandas', 'pyranges', 'scipy', 'matplotlib', 'statsmodels'],
+    entry_points={
+        'console_scripts': [
+            'epilogos = epilogos:main',
+        ],
+    }
+)

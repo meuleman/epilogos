7ffe75a86dc342a78d5d1a2fdae2ce0b4f04be51
Jacob Quon <jquon@sas.upenn.edu>
2022-08-24 23:42:44 -0700

    making sim search command line script

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     entry_points={
         "console_scripts": [
             "epilogos = epilogos.run:main",
+            "simsearch = epilogos.similarity_search:main"
         ],
     }
 )

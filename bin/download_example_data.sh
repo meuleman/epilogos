152e1920992192dd552a07145c90b6d7d54ec22d
Jacob Quon <jquon@sas.upenn.edu>
2022-09-26 14:50:46 -0700

    updated data download script to add simsearch data

--- bin/download_example_data.sh
+++ bin/download_example_data.sh
@@ -34,8 +34,20 @@ else
     echo ""
 fi
 
+if [[ ! -f data/simsearch/male/scores_male_s1_matrix_chr1.txt.gz ]]
+then
+    echo "Downloading similarity search data..."
+    mkdir -p data/simsearch/male/
+    curl -L https://github.com/meuleman/epilogos/raw/main/data/simsearch/male/scores_male_s1_matrix_chr1.txt.gz --output data/simsearch/male/scores_male_s1_matrix_chr1.txt.gz
+    echo
+else
+    echo "FILE NOT DOWNLOADED: data/simsearch/male/scores_male_s1_matrix_chr1.txt.gz already exists"
+    echo ""
+fi
+
 echo ""
 echo "Find files at:"
 echo "data/pyData/male/epilogos_matrix_chr1.txt.gz"
 echo "data/pyData/female/epilogos_matrix_chr1.txt.gz"
 echo "data/state_metadata/human/Boix_et_al_833_sample/hg19/18/metadata.tsv"
+echo "data/simsearch/male/scores_male_s1_matrix_chr1.txt.gz"
